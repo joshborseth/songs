3c5449458ebe3e6e6736bfcb62519c8c3d99e00b
joshborseth <joshuaborseth@gmail.com>
2024-02-19 12:06:52 -0800

    update schema

--- src/app/_components/Sidebar/Sidebar.tsx
+++ src/app/_components/Sidebar/Sidebar.tsx
@@ -9,12 +9,17 @@ import { Player } from "../Player";
 import { db } from "~/server/db";
 import { SongButton } from "./SongButton";
 import { Upload } from "../Upload";
+import { ScrollArea } from "~/components/ui/scroll-area";
 
 export async function Sidebar({ children }: { children: React.ReactNode }) {
   const songs = await db.query.songs.findMany();
   return (
     <div className="flex h-full">
-      <div className={cn("w-64 bg-white p-4 text-gray-900 shadow-2xl")}>
+      <div
+        className={cn(
+          "flex w-64 flex-col gap-2 bg-white p-4 text-gray-900 shadow-2xl",
+        )}
+      >
         <nav>
           <h1 className="pb-1 pl-3 text-3xl font-bold">Music</h1>
           <ul className="py-3">
@@ -28,14 +33,25 @@ export async function Sidebar({ children }: { children: React.ReactNode }) {
         </nav>
         <div className="py-4">
           <h2 className="pl-3 text-lg font-bold">Songs</h2>
-
-          <ul className="py-3">
-            {songs.map((song) => (
-              <SongButton key={song.id} song={song} />
-            ))}
-          </ul>
+          <ScrollArea className="h-56 py-3">
+            <ul>
+              {songs.map((song) => (
+                <SongButton key={song.id} song={song} />
+              ))}
+            </ul>
+          </ScrollArea>
         </div>
-        <div className="pl-3">
+        <div>
+          <h2 className="pl-3 text-lg font-bold">Playlists</h2>
+          <ScrollArea className="h-56 py-3">
+            <ul>
+              {songs.map((song) => (
+                <SongButton key={song.id} song={song} />
+              ))}
+            </ul>
+          </ScrollArea>
+        </div>
+        <div className="flex h-full items-end pl-3">
           <Upload />
         </div>
       </div>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,51 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { int, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 export const songs = mysqlTable("songs", {
   id: int("id").primaryKey().autoincrement(),
   name: varchar("name", { length: 256 }).notNull(),
-  createdAt: timestamp("created_at")
+  createdAt: timestamp("createdAt")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
   updatedAt: timestamp("updatedAt").onUpdateNow(),
-  s3Url: varchar("s3_url", { length: 256 }).notNull(),
-  imageUrl: varchar("image_url", { length: 256 }),
+  s3Url: varchar("s3Url", { length: 256 }).notNull(),
+  imageUrl: varchar("imageUrl", { length: 256 }),
 });
+
+export const playlists = mysqlTable("playlists", {
+  id: int("id").primaryKey().autoincrement(),
+  name: varchar("name", { length: 256 }).notNull(),
+  createdAt: timestamp("createdAt")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt").onUpdateNow(),
+});
+
+export const playlistSongs = mysqlTable("playlistSongs", {
+  id: int("id").primaryKey().autoincrement(),
+  playlistId: int("playlistId").notNull(),
+  songId: int("songId").notNull(),
+  createdAt: timestamp("createdAt")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt").onUpdateNow(),
+});
+
+export const playlistRelations = relations(playlists, ({ many }) => ({
+  playlistSongs: many(playlistSongs),
+}));
+
+export const songRelations = relations(songs, ({ many }) => ({
+  playlistSongs: many(playlistSongs),
+}));
+
+export const playlistSongRelations = relations(playlistSongs, ({ one }) => ({
+  song: one(songs, {
+    fields: [playlistSongs.songId],
+    references: [songs.id],
+  }),
+  playlist: one(playlists, {
+    fields: [playlistSongs.playlistId],
+    references: [playlists.id],
+  }),
+}));
