44b13a93e533cd737d9300d6b5309ae4b51d7759
joshborseth <joshuaborseth@gmail.com>
2024-06-03 22:12:40 -0700

    fix no results message not spanning entire table

--- src/app/_components/SongsTable/SongsTable.tsx
+++ src/app/_components/SongsTable/SongsTable.tsx
@@ -74,7 +74,10 @@ export const SongsTable = ({
             ))
           ) : (
             <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
+              <TableCell
+                colSpan={columns({ Actions }).length}
+                className="h-24 text-center"
+              >
                 No results.
               </TableCell>
             </TableRow>
