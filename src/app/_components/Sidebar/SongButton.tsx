4ef7cd8e0e107e4742b3c6cac2afadf27665bad7
joshborseth <joshuaborseth@gmail.com>
2024-06-12 21:00:29 -0700

    remove context menu from sidebar remove song from queue

--- src/app/_components/Sidebar/SongButton.tsx
+++ src/app/_components/Sidebar/SongButton.tsx
@@ -4,35 +4,28 @@ import { useAppState } from "~/app/stores/app";
 import { Button } from "~/components/ui/button";
 import { type songs } from "~/server/db/schema";
 import { SongName } from "../SongName";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTrigger,
-} from "~/components/ui/context-menu";
-import { Trash2 } from "lucide-react";
+import { ListMinus } from "lucide-react";
 
 export const SongButton = ({ song }: { song: typeof songs.$inferSelect }) => {
   const { setSong, removeSongFromQueue } = useAppState();
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <Button
-          className="w-full"
-          onClick={() => setSong(song)}
-          variant="ghost"
-        >
-          <SongName song={song} hideImage />
-        </Button>
-      </ContextMenuTrigger>
-      <ContextMenuContent>
-        <ContextMenuItem
-          onClick={() => removeSongFromQueue(song.id)}
-          className="flex gap-2"
-        >
-          <Trash2 size={16} /> <span>Remove from Queue</span>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <div className="flex min-w-0 max-w-full justify-between pr-2">
+      <Button
+        className="w-full min-w-0"
+        onClick={() => setSong(song)}
+        variant="ghost"
+      >
+        <SongName song={song} hideImage />
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => removeSongFromQueue(song.id)}
+        className="h-auto w-max p-3"
+      >
+        <ListMinus size={16} className="h-4 w-4 lg:h-auto lg:w-auto" />
+      </Button>
+    </div>
   );
 };
